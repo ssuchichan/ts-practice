c0d9dd68f681502e6189bd25758baa511a67f6ed
ssuchichan <ssuchichan@gmail.com>
2024-07-24 22:42:28 +0800

    update

---
+++ ch01/02basis.ts
@@ -0,0 +1,21 @@
+let a: number;
+a = 10;
+// a = 'hello'; // ERROR
+
+let b: string;
+b = 'hello';
+// b = 10;//ERROR
+
+let c: boolean = true;
+
+
+function sum(a: number, b: number) :number {
+    return a + b;
+}
+
+let result = sum(123,456);
+console.log(result);
+
+
+
+
